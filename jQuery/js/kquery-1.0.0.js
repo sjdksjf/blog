e060f4a3f66f7eac571de8337e9bfb8643e4acf5
MK <929293846@qq.com>
2018-06-05 21:07:04 +0800

    add test file

--- jQuery/js/kquery-1.0.0.js
+++ jQuery/js/kquery-1.0.0.js
@@ -5,9 +5,10 @@
          	return new kQuery.fn.init(selector);
          };
      //kQuery的原型对象
-      kQuery.fn =kQuery.prototype = {
+      kQuery.fn = kQuery.prototype = {
       	constructor:kQuery,
       	init: function(selector){
+      		selector = kQuery.trim(selector);
             //布尔值是假的情况返回空的jquery对象
       		if(!selector){
       			return this;
@@ -19,21 +20,66 @@
       	    	});
       	    	this[0] =document;
       	    	this.length = 1;
-      	    	return this;
-      	    	//选择器处理
-      	    }else{
+      	    	//处理字符串
+      	    }else if(kQuery.isString(selector)){
+      	    	//HTML代码处理
+      	    	if(kQuery.isHTML(selector)){
+      	    		var tmpDom=document.createElement('div');
+      	    		tmpDom.innerHTML = selector;
+
       	    	// 接收document上的选择器
-      	    	var doms = document.querySelectorAll(selector);
+      	    	/*var doms = document.querySelectorAll(selector);
                 // console.log(doms);
                 //循环页面上获取的数组加入到创建的数组中，然后返回出新数组。
                 for(var i =0; i<doms.length;i++){
                 	this[i] =doms[i];
                 }
                 this.length =doms.length;
-                return this;
+                return this;*/
+                [].push.apply(this,tmpDom.children);
+            //选择器处理     
+      	    }else{
+      	    	var doms = document.querySelectorAll(selector);
+      	    	[].push.apply(this,doms);
       	    }
+      	}  
+      	else if(kQuery.isArray(selector)){
+      		//由于apply转伪数组有兼容问题(IE8以下不兼容),所以把所有传入的数组转换成真数组
+      		var tmpArr =[].slice.call(selector);
+      	    //把转换后的真数组转换成伪数组	
+      	    [].push.apply(this,tmpArr);
+      	}else{
+      		this[0] =selector;
+      		this.length = 1;
       	}
-      }   
+      		return this;
+      }  
+     each : function(arr,fn){
+     	     //判断是不是数组
+             if(kQuery.isArray(arr)){
+                 for(var i=0,i<arr.length,i++){
+                    var res = fn.call(arr[i],i,arr[i]);
+                    if(res == false){
+                    	break;
+                    }else if(res == true){
+                    	continue;
+                    }
+                 }  
+             }else{
+             	for(key in arr){
+             		var res = fn.call(arr[i],i,arr[i]);
+             		if(res == false){
+                    	break;
+                    }else if(res == true){
+                    	continue;
+                    }
+             	}
+             }
+             return arr;
+     },
+      
+  }
+
 //判断是不是函数
 kQuery.isFunction = function(str){
 	return typeof str ==='function';
@@ -46,6 +92,20 @@ kQuery.isString = function(str){
 kQuery.isHTML = function(str){
 	//处理字符串
 	return str.charAt(0) == '<' && str.charAt(str.length-1) =='>' && str.length>=3;
+}
+kQuery.isArray =function(str){
+	return kQuery.isObject(str) && length in str;
+}
+kQuery.isObject=function(str){
+	return typeof str ==='object';
+}
+kQuery.trim =function(str){
+	if(kQuery.isString(str)){
+		//用正则去掉字符串的前后空格
+		return str.replace(/^\s+|\s+$/g,'');
+	}else{
+		return str;
+	}
 }
 
 kQuery.fn.init.prototype = kQuery.fn;
