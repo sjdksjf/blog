bbc008ea90a5c47fa513a12b15fe8ad5f5294d42
MK <929293846@qq.com>
2018-07-23 20:37:33 +0800

    add test file

--- 10-node.js/fs/001-fs.js
+++ 10-node.js/fs/001-fs.js
@@ -2,10 +2,8 @@
 const fs = require('fs');
 
 //打开文件
-fs.openSync(./001.txt,()=>{
-
-});
+fs.openSync('001.txt','r');
 //写入文件
-fs.writeSync(aaa,w);
+fs.writeSync('aaa','w');
 //保存并推出
 fs.closeSync(fd)
--- 10-node.js/stream/004-readable.js
+++ 10-node.js/stream/004-readable.js
@@ -1,3 +1,51 @@
-const readable = require('stream');
 
-console.log(readable);
+const EventEmitter = require('events');
+
+class MyEventEmitter extends EventEmitter{//继承events中的方法
+   constructor(data,offsetLength){//接收 new 这个函数传入的参数
+   	   super();
+       this.data =data;
+       this.offsetLength = offsetLength;
+       this.on('newListener',(eventName)=>{//new 的时候绑定事件，sit.on事件触发后执行该函数
+          if(eventName === 'data'){ //判断是否传入了data
+            console.log(this.listeners('data'));
+            //添加异步执行函数，同步执行时。传出的是空数组
+            setImmediate(()=>{//异步执行该方法
+			   this._dispatch();
+			 })
+          }
+
+       })
+
+   }
+
+   _dispatch(){
+   	     // 获取文件的length
+         let tentLength = this.data.length;
+         let sentLength = tentLength;
+         //按需要循环截取字符串 
+         while(sentLength>0){
+         	let sex =tentLength - sentLength;
+         	let end = sex + this.offsetLength;
+
+            let tmp = this.data.slice(sex,end);
+            let buf = Buffer.from(tmp);
+            this.emit('data',buf);
+            sentLength -= this.offsetLength; 
+         }
+         this.emit('end');   
+   }
+   
+}
+let data=`aaaaaaaaaabbbbbbbbbb`;
+const sit = new MyEventEmitter(data,10);
+
+let body = '';
+sit.on('data',(curser)=>{//绑定事件
+	//console.log('data...');
+    console.log(curser.toString());
+    //body += curser;
+})
+sit.on('end',()=>{
+	 console.log("end...");
+});
